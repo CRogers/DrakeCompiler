40cc25d9bf58c07fd7cdb1b0faa1e1334de6d92e
Callum Rogers <callumrogers@hotmail.co.uk>
2013-01-28 00:54:35 +0000

    Got parser to compile

--- BasicCompilerLib/Check.fs
+++ BasicCompilerLib/Check.fs
@@ -11,18 +11,3 @@ let isReturn = function
 let getReturnExpr = function
     | Return e -> e
     | _ -> failwith "Need return stmt"
-
-let checkReturnStmt (declA:DeclA) =
-    match declA.Item with
-        | Proc (name, prms, returnType, body) ->
-            // Check that all return statements return the correct type
-            foldAST (fun branch xss ->
-                let xs = List.concat xss
-                match branch.Item with
-                    | Return e -> if branch.PType = returnType then xs else xs @ [Error(sprintf "Incorrect return type for return expr. Expected type %s, got %s" (fmt returnType) (fmt branch.PType), branch.Pos)] 
-                    | _ -> xs) (fun x -> []) body
-
-let check program =
-    let concatMap f list = List.concat <| List.map f list
-    let errors = concatMap checkReturnStmt program
-    errors
